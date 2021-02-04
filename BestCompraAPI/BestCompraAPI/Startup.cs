677d704b1c5909929472aa382194787cc8aff77f
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-04 10:55:41 -0300

    API finalizada e funcionando

--- BestCompraAPI/BestCompraAPI/Startup.cs
+++ BestCompraAPI/BestCompraAPI/Startup.cs
@@ -30,6 +30,8 @@ namespace BestCompraAPI {
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }

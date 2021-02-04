415a1af979ee84b9abe4bdf914659741f0000560
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-04 10:28:48 -0300

    funciona

--- BestCompraAPI/BestCompraAPI/Controllers/TodoController.cs
+++ BestCompraAPI/BestCompraAPI/Controllers/TodoController.cs
@@ -36,5 +36,13 @@ namespace BestCompraAPI.Controllers {
 
             return todoItem;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item) {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
     }
 }

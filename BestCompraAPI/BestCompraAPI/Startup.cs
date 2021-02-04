d0a7b29e0a4844432d2cb3ae513045a8b480a81c
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-04 09:22:06 -0300

    registrando o contexto do banco de dados

--- BestCompraAPI/BestCompraAPI/Startup.cs
+++ BestCompraAPI/BestCompraAPI/Startup.cs
@@ -10,6 +10,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BestCompraAPI.Models;
 
 namespace BestCompraAPI {
     public class Startup {
@@ -21,6 +23,7 @@ namespace BestCompraAPI {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
